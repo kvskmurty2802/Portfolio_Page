bd0e7ee15acd7e48e35bcce21a15ac607463b88e
kvskmurty2802 <97388589+kvskmurty2802@users.noreply.github.com>
2024-06-01 23:53:27 +0530

    Update Projects.jsx

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -3,6 +3,7 @@ import ProjectBox from "./ProjectBox";
 import DummyJson from "../images/DummyJson.png";
 import PhoenixImage from "../images/PhoenixImage.png";
 import GymImage from "../images/Gym1.png";
+import APIImage from "../images/API.png"
 
 const Projects = () => {
   return (
@@ -11,8 +12,9 @@ const Projects = () => {
         My Projects
       </h1>
       <div className="project">
-        <ProjectBox projectPhoto={PhoenixImage} projectName="Phoenix" />
+        <ProjectBox projectPhoto={APIImage} projectName="API" />
         <ProjectBox projectPhoto={DummyJson} projectName="DummyJson" />
+        <ProjectBox projectPhoto={PhoenixImage} projectName="Phoenix" />
         <ProjectBox projectPhoto={GymImage} projectName="Gym" />
       </div>
     </div>
